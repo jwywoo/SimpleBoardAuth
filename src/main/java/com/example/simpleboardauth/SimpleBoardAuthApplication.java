8ca6c8561c2fb4ceefadb8399e2214c7a9bb6d35
jwywoo <jwywoo26@gmail.com>
2023-09-04 10:08:29 +0900

    First Commit/main: Project setting - build.gradle

---
+++ src/main/java/com/example/simpleboardauth/SimpleBoardAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleboardauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleboardauth/SimpleBoardAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleboardauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
